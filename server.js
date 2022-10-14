cbe1ac4e02453bc525771983728e132bb6f15551
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-14 00:48:44 -0500

    initial code in server.js file

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
 
-//PORT designation and the app expression
+//PORT designation and app expression
 const PORT = process.env.PORT || 3005;
 const app = express();
 
-//Express middleware
-app.use(express.urlencoded({ extended: false}));
+//Express.js middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'Hello World!'
+//   });
+// });
+
+
+/*
+Route to handle user requests that aren't supported by the app
+Because this is a catchall route, its placement is very important.
+If we place this route above other routes, this route will override
+all others—so make sure that this is the last one.
+*/
+app.use((req, res) => {
+  res.status(404).end();
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
