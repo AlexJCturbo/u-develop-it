f8697d759994f27a6d213af62f5796baa499d620
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-15 13:32:44 -0500

    created parties table and data

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,16 @@
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+/*Because TEXT is more lenient, it might be tempting to always use TEXT over VARCHAR.
+However, overuse of TEXT fields can bloat the database because MySQL will allocate
+the maximum amount of space for a TEXT value, no matter how long the value is. In
+SQL, it's important to keep the size of data in check as much as possible. If you
+know a field's data will be on the short side, always go with VARCHAR!*/
+
+
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
