30814363b6ea7c79a25d4c26cf201001b00de175
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-15 10:19:59 -0500

    added CRUD routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const inputCheck = require('./utils/inputCheck');
 
 //Import the mysql2 package
 const mysql = require('mysql2');
@@ -35,8 +36,23 @@ Once this method executes the SQL command, the callback function
 captures the responses from the query in two variables: err is the
 error response amd rows is the database query response
 */
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
+// Get all candidates
+//we use app to create endpoints
+app.get('/api/candidates/', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({error: err.message});
+      return;
+    }
+    /*Instead of logging the result, rows, from the database,
+    we'll send this response as a JSON object to the browser, using
+    res in the Express.js route callback.*/
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
 });
 /*
 The object that is returned from the application is an array of
@@ -44,21 +60,47 @@ objects, with each object representing a row of the candidates table.
 */
 
 //Get a single candidate
-db.query(`SELECT * FROM candidates WHERE id = 4`, (err, row) => {
-  if(err) {
-    console.log(err);
-  }
-  console.log(row);
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      //Error status code changed to 400 to notify client the request wasn't accepted.
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
 });
 
-//Delete a candidate
+//Delete a candidate using HTTP request method delete()
 // ? = placeholder
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//   if(err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// })
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        //result.affectedRows will verify whether any rows were changed
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
 /*
 The DELETE statement has a question mark (?) that denotes a placeholder,
 making this a prepared statement. A prepared statement can execute the
@@ -67,22 +109,35 @@ placeholder.
 */
 
 //Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-              //The four placeholders must match the four values in params, so we must use an array.
-const params = [7, 'Edward', 'Bellamy', 0];
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+    //The three placeholders must match the four values in params, so we must use an array.
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  //MySQL will autogenerate the id and relieve us of the responsibility to know which id is available to populate.
 /*
 We can't insert another candidate with id 1 again, because that id is
 already taken. This is why we received the error Duplicate entry '1'
 for key 'candidates.PRIMARY'. This is the PRIMARY KEY constraint at
 work, protecting the table from duplicate ids.
 */
-
-db.query(sql, params, (err, result) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result);
+  //Uses the parameters sql and params to create the candidate
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
 });
 
 /*
