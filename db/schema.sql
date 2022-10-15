346b6935776ca8da857caeb5eb2131a4552a50fe
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-15 16:02:39 -0500

    updated candidate routes to join party data

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,10 @@
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+/*When you're developing an application locally it's okay to drop and re-create databases
+and tables freely, and delete data as necessary. However, as soon as your application
+"goes live" to other developers, customers, or the general public, these operations
+become very risky.*/
+
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
@@ -15,5 +22,9 @@ CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
-  );
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+/*FOREIGN KEY tells SQL which table and field it references
+ON DELETE SET NULL to tell SQL to set a candidate's party_id field to NULL if a party is deleted */
