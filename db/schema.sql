3432290b29fbe089f92d2cfa245b0a1ca3055dfb
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-16 01:13:24 -0500

    created the votes table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
-DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 /*When you're developing an application locally it's okay to drop and re-create databases
 and tables freely, and delete data as necessary. However, as soon as your application
 "goes live" to other developers, customers, or the general public, these operations
@@ -27,7 +28,7 @@ CREATE TABLE candidates (
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
-/*FOREIGN KEY tells SQL which table and field it references
+/*we added a constrain as a FOREIGN KEY which is a field in one table that references the primary key of another table.
 ON DELETE SET NULL to tell SQL to set a candidate's party_id field to NULL if a party is deleted */
 
 CREATE TABLE voters (
@@ -44,3 +45,15 @@ CURRENT_TIMESTAMP will return the current date and time in the same 2020-01-01 1
 format. Note that the time will be based on what time it is according to the server, not
 the client's machine.
 So, in this code we're specifying CURRENT_TIMESTAMP as the value for DEFAULT.*/
+
+CREATE TABLE votes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+);
+/*uc_voter signifies that the values inserted into the voter_id field must be unique. For example, whoever has a voter_id of 1 can only appear in this table once.
+ON DELETE CASCADE, deleting the reference key will also delete the entire row from this table.*/
